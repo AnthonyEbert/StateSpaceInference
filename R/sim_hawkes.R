adfd37f0f5822e5bc9d081500d4fc5b19dd7e8bb
Anthony <anthonyebert@gmail.com>
2018-12-04 01:28:09 +1000

    Working completely.

---
+++ R/sim_hawkes.R
@@ -0,0 +1,63 @@
+
+#' @export
+decay_func <- function(x, alpha, delta){
+  if(length(x) == 0){
+    return(0)
+  }
+
+  out <- alpha * exp(-delta * x)
+  return(out)
+}
+
+#' Simulate inhomogenous Hawkes process
+#' @export
+#' @param lambda_fun function for inhomogenous poisson process
+#' @param history0 vector of historic times
+#' @param kern function Hawkes kernal
+#' @param startT numeric start time
+#' @param endT numeric end time
+#' @param maxI numeric maximum number of points to simulate
+#' @examples
+#' lambda_levels <- c(0.5, 1, 2, 1, 3, 0.5, 1, 1, 2, 0.5, 0.5, 0.5, 0.5, 2, 2, 2, 4, 0, 1, 1, 1)
+#' lambda_fun <- stepfun(seq(10, 200, by = 10), y = lambda_levels)
+#' kern <- function(x){return(decay_func(x, alpha = 1, delta = 1.2))}
+#'
+#' x <- sim_hawkes(lambda_fun, NULL, kern, 0, 200)
+#'
+#' hist(x, breaks = 200)
+#'
+#' plot(lambda_fun, add = TRUE, col = "red")
+sim_hawkes <- function(lambda_fun, history0 = NULL, kern, startT = max(history0), endT = 10, maxI = Inf, progressBar = TRUE){
+
+  TT = startT
+  i = 1
+  if(gtools::invalid(history0)){history0 <- NULL}
+  history <- history0
+
+  if(progressBar){
+    pb <- txtProgressBar(min = startT, max = endT, style = 3)
+  }
+
+  while(i <= maxI){
+    lambda_max <- lambda_fun(TT) + sum(kern(TT - history))
+
+    u <- runif(1, 0, 1)
+    tau <- -log(u) / lambda_max
+
+    TT <- TT + tau
+
+    s <- runif(1, 0, 1)
+    lambda_current <- lambda_fun(TT) + sum(kern(TT - history))
+
+    if(s <= lambda_current / lambda_max){
+      if(TT > endT){
+        return(history)
+      }
+      if(progressBar){setTxtProgressBar(pb, TT)}
+      history <- c(history, TT)
+      i <- i + 1
+    }
+  }
+
+  return(history)
+}
--- script/SMC2_ABC_run.R
+++ script/SMC2_ABC_run.R
@@ -1,9 +1,10 @@
 
 library(parallel)
-library(StateSpaceModel)
 sessionInfo()
 
 cl <- makeCluster(parallel::detectCores() - 1)
+
+library(StateSpaceInference)
 
 # IBIS Algorithm
 set.seed(3)
@@ -11,7 +12,7 @@ set.seed(3)
 # Prior :
 # sig ~ exp(1)
 # tau ~ exp(1)
-TT <- 40
+TT <- 20
 true_theta <- c(0.25, 0.5)
 #y <- data_simulator(true_theta, TT)
 
@@ -36,7 +37,7 @@ hist(y_history, breaks = TT * 10)
 
 plot(lambda_fun, add = TRUE, col = "red")
 
-simulator <- StateSpaceModel::hawkes_simulator
+simulator <- hawkes_simulator
 
 inp <- list(
   lower = lower,
@@ -47,31 +48,29 @@ inp <- list(
   simulator = simulator
 )
 
-library(StateSpaceInference)
-
 rstate <- function(n, theta, inp){
-  sapply(rep(NA, n), generate_statey, n = 1, lower = inp$lower, upper = inp$upper, sd = inp$sd_t, a = inp$a_logit)
+  sapply(rep(NA, n), generate_state, n = 1, lower = inp$lower, upper = inp$upper, sd = inp$sd_t, a = inp$a_logit)
 }
 
 loss = loss_hawkes
 
 control <- list(
-  Ntheta = 200,
+  Ntheta = 100,
   Nx = 200,
   pacc = 0.02
 )
 
 prior_sample <- matrix(rgamma(control$Ntheta, 10, 40), ncol = 1)
 
-# parallel <- ifelse(is.null(cl), 1,
-#                    ifelse("cluster" %in% class(cl), 2,
-#                           ifelse(cl == "mclapply", 3,
-#                                  ifelse(cl == "test", 4, 5)))
-# )
-#
-# lfunc <- StateSpaceInference:::make_listfunc(parallel, cl)
-
 x_list <- SMC2_ABC(prior_sample, dprior = dHawkes, rstate, loss, loss_args = inp, control = control, cl = cl, dt = 10, ESS_threshold = 0.5, TT = TT)
 
 plotrix::weighted.hist(sapply(x_list, function(x){x$theta}), w = sapply(x_list, function(x){x$omega})/sum(sapply(x_list, function(x){x$omega})), breaks = 100)
+
+q_mat <- array(unlist(x_list$q_l, recursive = FALSE), dim = c(3, 20))
+
+q_df <- data.frame(time = seq(1, 20), lower = q_mat[1,], med = q_mat[2,], upper = q_mat[3,], state = true_states)
+
+library(ggalt)
+
+ggplot(q_df) + aes(x = time, y = state, ymin = lower, ymax = upper) + geom_step() + geom_ribbon(alpha = 0.2, stat = "stepribbon", fill = "red") + geom_step(mapping = aes(x = time, y = med), col = "red") + ggthemes::theme_base() + scale_y_continuous(expand = c(0, 0)) + scale_x_continuous(expand = c(0, 0))
 
