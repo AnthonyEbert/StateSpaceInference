f276f5f655fe81581a22bc553c52e7a161eaadc8
Anthony <anthonyebert@gmail.com>
2018-12-07 16:19:33 +1000

    Switch to saving full_list and added functions for extracting useful output.

--- R/particle_filter.R
+++ R/particle_filter.R
@@ -1,8 +1,8 @@
 
 #' @export
 particle_filter <- function(theta, x, w, loss, loss_args, threshold, time1, time2, lfunc){
-  a <- sample(1:length(x), prob = w, replace = TRUE)
-  dist_out <- lapply(x[a], loss, theta = theta, inp = loss_args, time1 = time1, time2 = time2)
+  a <- sample(1:I(dim(x)[1]), prob = w, replace = TRUE)
+  dist_out <- apply(x[a, , drop = FALSE], MARGIN = 1, loss, theta = theta, inp = loss_args, time1 = time1, time2 = time2)
   return(dist_out)
 }
 
