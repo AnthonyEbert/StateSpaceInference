e4356761fc548b8dcd765434fda1ccfed7060520
Anthony <anthonyebert@gmail.com>
2018-12-07 16:19:33 +1000

    Switch to saving full_list and added functions for extracting useful output.

--- R/theta_filter.R
+++ R/theta_filter.R
@@ -4,11 +4,13 @@ theta_filter <- function(input_list, time1, time2, loss = loss, loss_args){
 
   output <- particle_filter(input_list$theta, input_list$x, input_list$w, loss = loss, loss_args = loss_args, time1 = time1, time2 = time2)
 
+  size_x <- length(output[[1]]$x)
+
   output_list <- input_list
 
   output_list$distance <- sapply(output, function(x){x$distance})
 
-  output_list$x <- sapply(output, function(x){x$x})
+  output_list$x <- matrix(sapply(output, function(x){x$x}), byrow = TRUE, ncol = size_x)
 
   return(output_list)
 }
