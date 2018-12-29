1d2640339dda7bddb5be098e110ee3f055738958
Anthony Ebert <anthonyebert@gmail.com>
2018-12-29 23:31:19 +1000

    Hawkes_28 first try of 0.5.0

--- script/SMC2_ABC_run.R
+++ script/SMC2_ABC_run.R
@@ -11,7 +11,7 @@ cl = "mclapply"
 
 set.seed(3)
 
-TT <- 15
+TT <- 20
 true_theta <- c(0.5, 0.5)
 lower <- 0
 upper <- 3.5
@@ -52,8 +52,8 @@ inp <- list(
 loss = loss_hawkes
 
 
-Ntheta = 200
-Nx = 100000
+Ntheta = 100
+Nx = 10000
 pacc = 5e-4
 
 lower_theta <- c(0.3, 0.3)
@@ -81,7 +81,7 @@ invtrans <- function(x, trans_args){
   return(cbind(theta1, theta2))
 }
 
-full_list <- SMC2_ABC(prior_sample, dprior = dHawkes, loss, loss_args = inp, Ntheta = Ntheta, Nx = Nx, pacc = pacc, cl = cl, dt = 10, ESS_threshold = 0.5, TT = TT, trans = trans, invtrans = invtrans, trans_args = trans_args)
+full_list <- SMC2_ABC(prior_sample, dprior = dHawkes, loss, loss_args = inp, Ntheta = Ntheta, Nx = Nx, pacc = pacc, cl = cl, dt = 10, ESS_threshold = 0.2, TT = TT, trans = trans, invtrans = invtrans, trans_args = trans_args)
 
 state_df <- get_state(full_list, probs = c(0.25, 0.5, 0.75))
 
