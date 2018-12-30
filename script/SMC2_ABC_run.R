ed24f5c039ef6cecedd88fe16d43b77f1980f3c4
Anthony Ebert <anthonyebert@gmail.com>
2018-12-30 22:27:51 +1000

    Hawkes_42

--- script/SMC2_ABC_run.R
+++ script/SMC2_ABC_run.R
@@ -75,7 +75,7 @@ invtrans <- function(x, trans_args){
   return(cbind(theta1, theta2))
 }
 
-full_list <- SMC2_ABC(prior_sample, dprior = dHawkes, loss, loss_args = inp, Ntheta = Ntheta, Nx = Nx, pacc = pacc, cl = cl, dt = 10, ESS_threshold = 0.5, TT = TT, trans = trans, invtrans = invtrans, cov_coef = 0.2)
+full_list <- SMC2_ABC(prior_sample, dprior = dHawkes, loss, loss_args = inp, Ntheta = Ntheta, Nx = Nx, pacc = pacc, cl = cl, dt = 10, ESS_threshold = 0.5, TT = TT, trans = trans, invtrans = invtrans, cov_coef = 0.25^2)
 
 state_df <- get_state(full_list, probs = c(0.25, 0.5, 0.75))
 
