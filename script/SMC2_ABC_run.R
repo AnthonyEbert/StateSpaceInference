b9f06b83e41d17af43df4017a5a6ad6baa68447d
Anthony Ebert <anthonyebert@gmail.com>
2019-01-07 15:34:57 +1000

    Hawkes_60 version 1.2.1

--- script/SMC2_ABC_run.R
+++ script/SMC2_ABC_run.R
@@ -5,8 +5,8 @@ library(ggplot2)
 library(ggalt)
 sessionInfo()
 
-cl <- makeCluster(parallel::detectCores())
-#cl = "mclapply"
+#cl <- makeCluster(parallel::detectCores())
+cl = "mclapply"
 #cl <- NULL
 
 set.seed(3)
@@ -54,8 +54,8 @@ inp <- list(
 loss = loss_hawkes
 
 
-Ntheta = 40
-Nx = 400
+Ntheta = 200
+Nx = 1000
 pacc = 0.05
 
 lower_theta <- c(0.3, 0.4)
@@ -85,6 +85,13 @@ state_df$state <- true_states
 
 theta_df <- get_parameter(full_list)
 
+
+save.image()	
+save(state_df, file = "state_df.RData")	
+save(theta_df, file = "theta_df.RData")
+
+
+
 ggplot(state_df) + aes(x = time, y = state, ymin = lower, ymax = upper) + geom_step() + geom_ribbon(alpha = 0.2, stat = "stepribbon", fill = "red") + geom_step(mapping = aes(x = time, y = med), col = "red") + ggthemes::theme_base() + scale_y_continuous(expand = c(0, 0)) + scale_x_continuous(expand = c(0, 0))
 
-ggplot(theta_df) + aes(x = value, weights = weight, col = factor(time)) + geom_density() + facet_wrap(~parameter, scales = "free")
+ggplot(theta_df[which(theta_df$time %% 5 == 0),]) + aes(x = value, weights = weight, col = factor(time)) + geom_density() + facet_wrap(~parameter, scales = "free")
