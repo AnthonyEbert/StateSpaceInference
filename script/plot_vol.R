e015a39f4c5e266c6a071ee4de560fb2b5dcf646
Anthony <anthonyebert@gmail.com>
2019-01-09 17:40:06 +1000

    plot_vol

---
+++ script/plot_vol.R
@@ -0,0 +1,47 @@
+library(ggplot2)
+library(ggalt)
+library(dplyr)
+
+load("state_df.RData")
+load("theta_df.RData")
+
+fullpath = getwd()
+directoryname = basename(fullpath)
+
+nr <- dim(state_df)[1]
+
+state_df <- state_df %>%
+  bind_rows(state_df[nr,])
+
+state_df$time[nr] <- state_df$time[nr] + 1
+
+state_plot <- ggplot(state_df) +
+  aes(x = time - 1, y = med, ymin = lower, ymax = upper) +
+  geom_step() +
+  geom_ribbon(alpha = 0.2, stat = "stepribbon") +
+  geom_step(mapping = aes(x = time - 1, y = state), col = "red") +
+  ggthemes::theme_base() +
+  xlab("Time: t") +
+  ylab(expression(State:~x[j]))
+
+ggsave(paste0("Gaussian_", directoryname, "_state.pdf"), height = 10, width = 15, units = "cm", plot = state_plot)
+
+theta_of_interest <- theta_df[which(theta_df$time == max(theta_df$time)),] %>% select(-time, -parameter)
+
+limits <- data.frame(value = c(-1,1), weight = 0)
+
+theta_of_interest <- bind_rows(theta_of_interest, limits)
+
+true_theta <- data.frame(value = c(0.95))
+
+parameter_plot <- ggplot(theta_of_interest) +
+  aes(x = value, weights = weight) +
+  geom_density(adjust = 2) +
+  geom_vline(data = true_theta, mapping = aes(xintercept = value), col = "red") +
+  xlab(expression(Parameter~value:~phi)) +
+  scale_y_continuous(expand = expand_scale(mult = c(0,0.05))) +
+  ggthemes::theme_few() +
+  theme(axis.text.y = element_blank(), axis.ticks.y = element_blank(), axis.line.y = element_blank()) +
+  ylab("ABC posterior density")
+
+ggsave(paste0("Gaussian_", directoryname, "_parameter.pdf"), height = 7, width = 10, units = "cm", plot = parameter_plot)
