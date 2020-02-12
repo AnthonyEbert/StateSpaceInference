374b4821a4dfb4b11541e624a3dbf9ef9dc9b45e
Anthony Ebert <anthonyebert@gmail.com>
2020-02-12 11:02:52 +0100

    First stan run. Vol_4
    https://mc-stan.org/docs/2_22/stan-users-guide/stochastic-volatility-models.html

--- script/stan/stan.R
+++ script/stan/stan.R
@@ -4,26 +4,36 @@ library(parallel)
 library(rstan)
 
 sessionInfo()
-
-#cl <- makeCluster(parallel::detectCores() - 1)
-#cl = "mclapply"
-cl <- NULL
+set.seed(1)
 
 # length of the time series
 TT <- 40
 # parameters
-mu <- -0.2; phi <- 0.7; sh <- 0.6
+mu <- -0.2; phi <- 0.95; sh <- 0.6
 # simulating the hidden states
 h <- rep(0, TT)
-h[1] <- rnorm(1)
+h[1] <- rnorm(1, mu, sh/(sqrt(1-phi^2)))
 for (t in 2:TT) {
-  h[t] <- mu + phi * h[t - 1] + sh * rnorm(1)
+  h[t] <- mu + phi * (h[t - 1]) + sh * rnorm(1)
 }
 
 # emission of the observations
 yobs <- exp(h/2) * rnorm(TT, 0, 1)
 
 dat <- list(TT = TT, y = yobs, mu = mu, sh = sh)
-fit <- stan(file = "script/stan/stochvol.stan", model_name = "example",
-            data = dat, iter = 100, chains = 3)
+fit <- stan(
+  file = "../../../script/stan/stochvol.stan",
+  model_name = "example",
+  data = dat,
+  iter = 50000,
+  chains = parallel::detectCores(),
+  cores = parallel::detectCores()
+)
 print(fit)
+theta_stan = extract(fit, pars = "theta")$theta
+
+save.image()
+save(theta_stan, file = "theta_stan.RData")
+
+
+#init = rep(list(list(theta = phi)), parallel::detectCores())
--- script/stan/stochvol.stan
+++ script/stan/stochvol.stan
@@ -6,12 +6,20 @@ data {
 }
 parameters {
   real<lower=-1> theta;         // white noise shock scale
-  vector[TT] x;                 // log volatility at time t
+  vector[TT] x_std;                 // log volatility at time t
+}
+transformed parameters {
+  vector[TT] x = x_std * sh;  // now h ~ normal(0, sigma)
+  x[1] /= sqrt(1 - theta * theta);  // rescale h[1]
+  x += mu;
+  for (t in 2:TT)
+    x[t] += theta * (x[t-1]);
 }
 model {
   theta ~ uniform(-1, 1);
-  x[1] ~ normal(mu/(1-theta), sh^2/(1-theta^2));
-  for (t in 2:TT)
-    x[t] ~ normal(mu + theta * x[t-1], sh);
+  x_std ~ std_normal();
+  // x[1] ~ normal(mu/(1-theta), sh^2/(1-theta^2));
+  // for (t in 2:TT)
+  //   x[t] ~ normal(mu + theta * x[t-1], sh);
   y ~ normal(0, exp(0.5 * x));
 }
