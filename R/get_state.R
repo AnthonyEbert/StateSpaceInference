7e7c33d9cfac36e4f09d6524e904f461d7faf2ed
AnthonyEbert <anthonyebert@gmail.com>
2020-04-27 10:48:07 +0200

    streamline get_state

--- R/get_state.R
+++ R/get_state.R
@@ -11,35 +11,23 @@ get_state <- function(full_list, dim = 1, probs = c(0.025, 0.5, 0.975), cl = NUL
 
   q_mat <- list()
 
-  if(is.na(probs[1])){
-    x_array <- array(dim = c(TT, Ntheta, Nx))
-    w_array <- array(dim = c(TT, Ntheta, Nx))
-  }
-
-  output <- parallel::parLapply(cl, full_list, function(x_list){
+  output <- parallel::parLapply(cl, full_list, function(x_list, probs){
 
     x_mat <- t(abind::abind(lapply(x_list, function(x){abind::abind(x$x, along = 2)})))
 
     w_mat <- t(sapply(x_list, function(x){x$w * x$omega}))
 
-    if(is.na(probs[1])){
-      x_array[tp,,] <- x_mat
-      w_array[tp,,] <- w_mat
-    }
+    #x <- as.numeric(x_mat)
+    w <- as.numeric(w_mat)
 
-    if(!is.na(probs[1])){
-      #x <- as.numeric(x_mat)
-      w <- as.numeric(w_mat)
+    # state_sample <- x_mat[indicies,]
 
-      # state_sample <- x_mat[indicies,]
+    q_mat <- apply(x_mat, 2, Hmisc::wtd.quantile, weights = w/sum(w) * 1e6, probs = probs, normwt = FALSE)
 
-      q_mat <- apply(x_mat, 2, Hmisc::wtd.quantile, weights = w/sum(w) * 1e6, probs = probs, normwt = FALSE)
-
-      #q_mat[tp,] <- Hmisc::wtd.quantile(x, weights = w / sum(w), probs = probs, normwt = FALSE)
-    }
+    #q_mat[tp,] <- Hmisc::wtd.quantile(x, weights = w / sum(w), probs = probs, normwt = FALSE)
 
     return(q_mat)
-  })
+  }, probs = probs)
 
   # if(!is.na(probs[1])){
   #   output <- data.frame(time = seq(1, TT), lower = q_mat[,1], med = q_mat[,2], upper = q_mat[,3])
