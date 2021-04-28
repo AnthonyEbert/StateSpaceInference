03f25bcac3a8c57fd5604388dae2659ce19a9c2a
AnthonyEbert <anthonyebert@gmail.com>
2021-04-28 13:50:02 +0200

    stan_skew change to skew normal

--- R/loss_simple.R
+++ R/loss_simple.R
@@ -43,5 +43,32 @@ generate_stan_skew <- function(TT, true_states, theta){
   return(y)
 }
 
+#' @export
+loss_stan_skew <- function(x, theta, time1, time2, inp){
+  x <- as.numeric(x[1])
+
+  if(gtools::invalid(x)){
+    x <- rnorm(1)
+  } else {
+    x <- rnorm(1, mean = x)
+  }
+
+  y <- suppressMessages(sn::rsn(1e1, x, theta[1], theta[2]))
+
+  ss_obs <- c(mean(inp$y[[time2]]), sd(inp$y[[time2]]), e1071::skewness(inp$y[[time2]]))
+  ss_sim <- c(mean(y), sd(y), e1071::skewness(y))
+
+  # ss_obs <- sn::summary(sn::selm(inp$y[[time2]]~1, opt.method = "BFGS"))@param.table[,1]
+  # ss_sim <- sn::summary(sn::selm(y~1), opt.method = "BFGS")@param.table[,1]
+
+  if(is.null(inp$weights)){
+    inp$weights <- c(1,1,1)
+  }
+
+  distance_out <- sum( (ss_obs - ss_sim)^2 / inp$weights^2 )
+
+  return(list(distance = sqrt(distance_out), x = x))
+}
+
 
 
