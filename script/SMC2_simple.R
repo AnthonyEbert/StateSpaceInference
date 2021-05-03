207c890077c3938637da7bbdc1c1da4fc3ac8d7c
AnthonyEbert <anthonyebert@gmail.com>
2021-05-03 11:26:01 +0200

    Fixes.
    Start Gaussian 17.

--- script/SMC2_simple.R
+++ script/SMC2_simple.R
@@ -75,11 +75,13 @@ state_df$state <- true_states
 
 theta_df <- get_parameter(full_list)
 
-save.image()
+save(theta_df, file = "theta_df.RData")
+
+ggplot(theta_df[which(theta_df$time %% 5 == 0),]) + aes(x = value, weights = weight, col = factor(time)) + geom_density() + facet_wrap(~parameter, scales = "free")
 
 theta_df <- theta_df %>%
   filter(time == TT) %>%
-  mutate(seed = seed, type = "ABC") %>%
+  mutate(type = "ABC") %>%
   select(-parameter, -time)
 
 saveRDS(state_df, file = "state_df.rds")
@@ -87,6 +89,6 @@ saveRDS(theta_df, file = "theta_df.rds")
 
 
 
-ggplot(state_df) + aes(x = time, y = state, ymin = lower, ymax = upper) + geom_step() + geom_ribbon(alpha = 0.2, stat = "stepribbon", fill = "red") + geom_step(mapping = aes(x = time, y = med), col = "red") + scale_y_continuous(expand = c(0, 0)) + scale_x_continuous(expand = c(0, 0))
+#ggplot(state_df) + aes(x = time, y = state, ymin = lower, ymax = upper) + geom_step() + geom_ribbon(alpha = 0.2, stat = "stepribbon", fill = "red") + geom_step(mapping = aes(x = time, y = med), col = "red") + scale_y_continuous(expand = c(0, 0)) + scale_x_continuous(expand = c(0, 0))
 
-ggplot(theta_df[which(theta_df$time %% 5 == 0),]) + aes(x = value, weights = weight, col = factor(time)) + geom_density() + facet_wrap(~parameter, scales = "free")
+
